fa75969734cba3ca66ffa5d74a9b1cb268cb8195
Ahmad Hasan <58470792+ahmad649@users.noreply.github.com>
2025-02-12 16:29:50 -0500

    Create MapWriter.java

---
+++ src/maps/MapWriter.java
@@ -0,0 +1,22 @@
+package maps;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * MapWriter handles saving the map to a file.
+ */
+class MapWriter {
+    
+    public MapWriter() {
+        // Constructor
+    }
+    
+    /**
+     * Saves the map to a specified file.
+     * @param filename Name of the file to save.
+     */
+    public void saveMap(String filename) {
+        // TODO: Implement saving logic
+    }
+}
