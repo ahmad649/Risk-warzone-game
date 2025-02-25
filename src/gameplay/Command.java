e6e9d1f954fd5754207872df7b3937f39e2daeb2
handikaharianto <handikaharianto01@gmail.com>
2025-02-24 23:21:08 -0500

    Update Command constructor to accept multiple arguments

--- src/gameplay/Command.java
+++ src/gameplay/Command.java
@@ -3,154 +3,51 @@ package gameplay;
 import java.util.*;
 
 public class Command{
-    Integer numArgs;
-    String commandType;
+    Integer d_numArgs;
+    String d_commandType;
     //Used when args are in -argname actual_arg format
-    HashMap<String,String> argsLabled;
-    ArrayList<String> argArr;
+    HashMap<String,List<String>> d_argsLabeled;
+    ArrayList<String> d_argArr;
 
-    public Command(String input){
-        String[] parts = input.split(" ");
-        commandType = parts[0];
-        argsLabled = new HashMap<>();
-        argArr = new ArrayList<>();
-        for (int i = 1;i< parts.length;i+=1){
-            if (parts[i].charAt(0)=='-'){
-                argsLabled.put(parts[i],parts[i+1]);
-                i+=1;
+    public Command(String p_input) {
+        // Split the string
+        String[] l_parts = p_input.split(" ");
+
+        d_commandType = l_parts[0];
+        d_argsLabeled = new HashMap<>();
+        d_argArr = new ArrayList<>();
+        // Iterate through the split commands
+        for (int l_i = 1; l_i < l_parts.length; l_i++) {
+            // Check for flag
+            if (l_parts[l_i].startsWith("-")) {
+                String l_flag = l_parts[l_i];
+                List<String> l_values = new ArrayList<>();
+
+                // Skip flag
+                int l_j = l_i + 1;
+
+                // Ensure the current index doesn't exceed the length of split commands and it doesn't start with '-'
+                while (l_j < l_parts.length && !l_parts[l_j].startsWith("-")) {
+                    // Add arguments to a list
+                    l_values.add(l_parts[l_j]);
+                    l_j++;
+                }
+
+                // Add to key-value pairs to hashmap
+                d_argsLabeled.put(l_flag, l_values);
+                l_i = l_j - 1; // Move i to the end of the processed arguments
             } else {
-                argArr.add(parts[i]);
+                d_argArr.add(l_parts[l_i]);
             }
         }
-        numArgs = argArr.size();
+        d_numArgs = d_argArr.size();
     }
 
-    public static HashMap<String, List<String>> parse_editcontinent_command(String p_command){
-        // Split command into an array of strings
-        String[] l_parts = p_command.split(" ");
-
-        // Use hashmap to group arguments
-        HashMap<String , List<String>> l_arguments = new HashMap<>();
-        for (int l_i = 0; l_i < l_parts.length; l_i++) {
-            switch (l_parts[l_i]) {
-                case "-add":
-                    // Add 'continentID' and 'continentValue' to hashmap
-                    l_arguments.put("add", new ArrayList<>(Arrays.asList(l_parts[l_i + 1], l_parts[l_i + 2])));
-                    break;
-                case "-remove":
-                    // Add 'continentID' to hashmap
-                    l_arguments.put("remove", new ArrayList<>(Collections.singletonList(l_parts[l_i + 1])));
-            }
-        }
-
-        return l_arguments;
+    public HashMap<String, List<String>> getArgsLabeled() {
+        return d_argsLabeled;
     }
 
-    public static HashMap<String, List<String>> parse_editcountry_command(String p_command){
-        // Split command into an array of strings
-        String[] l_parts = p_command.split(" ");
-
-        // Use hashmap to group arguments
-        HashMap<String , List<String>> l_arguments = new HashMap<>();
-        for (int l_i = 0; l_i < l_parts.length; l_i++) {
-            switch (l_parts[l_i]) {
-                case "-add":
-                    // Add 'countryID' and 'continentID' to hashmap
-                    l_arguments.put("add", new ArrayList<>(Arrays.asList(l_parts[l_i + 1], l_parts[l_i + 2])));
-                    break;
-                case "-remove":
-                    // Add 'countryID' to hashmap
-                    l_arguments.put("remove", new ArrayList<>(Collections.singletonList(l_parts[l_i + 1])));
-            }
-        }
-
-        return l_arguments;
-    }
-
-    public static HashMap<String, List<String>> parse_editneighbor_command(String p_command){
-        // Split command into an array of strings
-        String[] l_parts = p_command.split(" ");
-
-        // Use hashmap to group arguments
-        HashMap<String , List<String>> l_arguments = new HashMap<>();
-        for (int l_i = 0; l_i < l_parts.length; l_i++) {
-            switch (l_parts[l_i]) {
-                case "-add":
-                    // Add 'countryID' and 'neighborCountryID' to hashmap
-                    l_arguments.put("add", new ArrayList<>(Arrays.asList(l_parts[l_i + 1], l_parts[l_i + 2])));
-                    break;
-                case "-remove":
-                    // Add 'countryID' and 'neighborCountryID' to hashmap
-                    l_arguments.put("remove", new ArrayList<>(Arrays.asList(l_parts[l_i + 1], l_parts[l_i + 2])));
-            }
-        }
-
-        return l_arguments;
-    }
-
-    public static String parse_savemap_command(String p_command){
-        // Split command into an array of strings
-        String[] l_parts = p_command.split(" ");
-
-        // Return file name
-        return l_parts[1];
-    }
-
-    public static String parse_editmap_command(String p_command){
-        // Split command into an array of strings
-        String[] l_parts = p_command.split(" ");
-
-        // Return file name
-        return l_parts[1];
-    }
-
-    public static String parse_loadmap_command(String p_command){
-        // Split command into an array of strings
-        String[] l_parts = p_command.split(" ");
-
-        // Return file name
-        return l_parts[1];
-    }
-
-    public static HashMap<String, List<String>> parse_gameplayer_command(String p_command){
-        // Split command into an array of strings
-        String[] l_parts = p_command.split(" ");
-
-        // Use hashmap to group arguments
-        HashMap<String , List<String>> l_arguments = new HashMap<>();
-        for (int l_i = 0; l_i < l_parts.length; l_i++) {
-            switch (l_parts[l_i]) {
-                case "-add":
-                    // Create 'add' as key in hashmap if it doesn't exist yet
-                    if (!l_arguments.containsKey("add")){
-                        l_arguments.put("add", new ArrayList<>());
-                    }
-                    // Add 'playerName' to list with the key 'add'
-                    l_arguments.get("add").add(l_parts[l_i + 1]);
-                    break;
-                case "-remove":
-                    // Create 'remove' as key in hashmap if it doesn't exist yet
-                    if (!l_arguments.containsKey("remove")){
-                        l_arguments.put("remove", new ArrayList<>());
-                    }
-                    // add 'playerName' to list with the key 'remove'
-                    l_arguments.get("remove").add(l_parts[l_i + 1]);
-                    break;
-            }
-        }
-
-        return l_arguments;
-    }
-
-    public static HashMap<String, String> parse_deploy_command(String p_command){
-        // Split command into an array of strings
-        String[] l_parts = p_command.split(" ");
-
-        // Create hashmap to store 'countryID' and 'num'
-        HashMap<String, String> l_arguments = new HashMap<>();
-        l_arguments.put("countryID", l_parts[1]);
-        l_arguments.put("num", l_parts[2]);
-
-        return l_arguments;
+    public ArrayList<String> getArgArr() {
+        return d_argArr;
     }
 }
