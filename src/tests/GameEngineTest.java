a7917a0bcb1ea0ee91bf91d71688986a5733b09c
handikaharianto <handikaharianto01@gmail.com>
2025-03-06 11:04:34 -0500

    Add test cases for GameEngine class

---
+++ src/tests/GameEngineTest.java
@@ -0,0 +1,86 @@
+package tests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import gameplay.GameEngine;
+import gameplay.Order;
+import gameplay.Player;
+import maps.MapReader;
+import model.Country;
+
+import static org.junit.Assert.*;
+
+public class GameEngineTest {
+    private Player d_player1, d_player2;
+    private List<Player> d_playersList = new ArrayList<>();
+    private final GameEngine d_gameEngine = new GameEngine();
+    private final MapReader d_mapReader = new MapReader();
+
+    /**
+     * Initialize all test cases by loading Game Map, creating players, and assigning countries to all players
+     */
+    @Before
+    public void initialize() {
+        // Load map and set countries
+        System.out.println("\nLoading Map...");
+        d_mapReader.loadMap("Classic_World_Map");
+        List<Country> l_countryList = d_mapReader.getCountriesMap().values().stream().toList();
+        d_gameEngine.setCountryList(l_countryList);
+
+        d_player1 = new Player("TestPlayer1");
+        d_player2 = new Player("TestPlayer2");
+
+        d_gameEngine.getPlayersList().add(d_player1);
+        d_gameEngine.getPlayersList().add(d_player2);
+
+        d_gameEngine.assignCountries();
+    }
+
+    /**
+     * Calculate reinforcement armies.
+     */
+    @Test
+    public void calculateReinforcementArmies() {
+        System.out.println("\nTEST : Calculate the number of reinforcement armies\n");
+
+        d_gameEngine.assignReinforcements();
+
+        int l_player1_armies = 5;
+        int l_player2_armies = 5;
+
+        System.out.println("\n-> Total number of reinforcement armies for " + d_player1.getName() + " : " + d_player1.getReinforcements());
+        System.out.println("-> Total number of reinforcement armies for " + d_player2.getName() + " : " + d_player2.getReinforcements());
+
+        assertEquals(l_player1_armies, d_player1.getReinforcements());
+        assertEquals(l_player2_armies, d_player2.getReinforcements());
+    }
+
+    /**
+     * Deploy more armies than the available reinforcement pool.
+     */
+    @Test
+    public void deployMoreArmiesThanReinforcementPool() {
+        System.out.println("\nTEST : Disallow deploying more armies than the reinforcement pool\n");
+
+        d_gameEngine.assignReinforcements();
+
+        int l_player1_deploy_armies = 7;
+        int l_player2_deploy_armies = 10;
+
+        // Deploy armies
+        System.out.println();
+        Order l_player1_order = new Order("deploy", "Ural", l_player1_deploy_armies, d_player1);
+        l_player1_order.execute();
+
+        System.out.println();
+        Order l_player2_order = new Order("deploy", "Central America", l_player2_deploy_armies, d_player2);
+        l_player2_order.execute();
+
+        assertNotEquals(l_player1_deploy_armies, d_player1.getReinforcements());
+        assertNotEquals(l_player2_deploy_armies, d_player2.getReinforcements());
+    }
+}
