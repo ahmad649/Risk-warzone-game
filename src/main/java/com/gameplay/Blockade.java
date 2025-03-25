55b2fae931ef1f6307aed0ed6fc34e02a404884a
handikaharianto <handikaharianto01@gmail.com>
2025-03-24 22:14:04 -0400

    Add test cases for Blockade order

--- src/main/java/com/gameplay/Blockade.java
+++ src/main/java/com/gameplay/Blockade.java
@@ -3,43 +3,51 @@ package com.gameplay;
 import com.model.Country;
 
 public class Blockade extends Order {
+    Country l_countryToBlockade;
+    int l_numOfArmies;
 
     public Blockade(Player p_player, String p_countryName) {
         super("blockade", p_countryName, 0, p_player);
     }
 
-    @Override
-    public void execute() {
+    public boolean isValid() {
         // Check if the player owns a blockade card
         if (!d_player.getD_cards().contains(Card.BLOCKADE)) {
             System.out.println("\nError: Player " + d_player.getName() + " does not own " + Card.BLOCKADE + " card");
-            return;
+            return false;
         }
 
         // Check if the specified country is owned by the current player
-        Country l_countryToBlockade = d_player.getCountryByName(d_countryName);
-        if (l_countryToBlockade == null) {
+        this.l_countryToBlockade = d_player.getCountryByName(d_countryName);
+        if (this.l_countryToBlockade == null) {
             System.out.println("\nError: Player " + d_player.getName() + " does not own " + d_countryName + " country");
-            return;
+            return false;
         }
 
-        int l_numOfArmies = l_countryToBlockade.getArmies();
+        this.l_numOfArmies = this.l_countryToBlockade.getArmies();
         // Check the number of armies in the specified country
-        if (l_numOfArmies <= 0) {
+        if (this.l_numOfArmies <= 0) {
             System.out.println("\nError: Country " + d_countryName + " has 0 armies");
-            return;
+            return false;
         }
 
-        // Triple the number of armies on the specified country
-        l_countryToBlockade.setArmies(l_numOfArmies * 3);
-        System.out.println("\nSuccess: Now country " + d_countryName + " has " + l_numOfArmies + " armies");
+        return true;
+    }
 
-        // Remove the specified country from the current player's countries
-        d_player.removeCountry(d_countryName);
+    @Override
+    public void execute() {
+        if (this.isValid()) {
+            // Triple the number of armies on the specified country
+            this.l_countryToBlockade.setArmies(l_numOfArmies * 3);
+            System.out.println("\nSuccess: Now country " + d_countryName + " has " + this.l_numOfArmies + " armies");
 
-        // TODO:Convert the specified country into neutral territory
+            // Remove the specified country from the current player's countries
+            d_player.removeCountry(d_countryName);
 
-        // Remove blockade card from the current player
-        d_player.getD_cards().remove(Card.BLOCKADE);
+            // TODO:Convert the specified country into neutral territory
+
+            // Remove blockade card from the current player
+            d_player.getD_cards().remove(Card.BLOCKADE);
+        }
     }
 }
